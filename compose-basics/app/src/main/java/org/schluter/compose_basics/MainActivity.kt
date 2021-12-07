784e218a249e3da282702c9d4b8482bf957db17d
leonardoschluter <leonardoschluter@gmail.com>
2021-12-07 18:28:30 -0300

    Create row to present button

--- compose-basics/app/src/main/java/org/schluter/compose_basics/MainActivity.kt
+++ compose-basics/app/src/main/java/org/schluter/compose_basics/MainActivity.kt
@@ -3,12 +3,12 @@ package org.schluter.compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,10 +32,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String = "") {
-    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.fillMaxWidth(1f).padding(8.dp)) {
-        Column(modifier = padding_16) {
-            Text(text = "Hello,")
-            Text(text = "$name!")
+    Surface(color = MaterialTheme.colors.primary, modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        Row(modifier = padding_16) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = { /*TODO*/ }) {
+                    Text("Show more")
+            }
         }
     }
 }
